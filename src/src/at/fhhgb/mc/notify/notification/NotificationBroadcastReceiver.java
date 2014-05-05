0b512d6eccaa926557fb0559a23058eadd73889b
Florian Schrofner <florian.schrofner@outlook.com>
2014-05-05 23:44:21 +0200

    started implementation for notification service

---
+++ src/src/at/fhhgb/mc/notify/notification/NotificationBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package at.fhhgb.mc.notify.notification;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationBroadcastReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
