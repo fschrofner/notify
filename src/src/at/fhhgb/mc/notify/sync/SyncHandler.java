e71608ff8e86613b87e9a14b0767a901939026f9
Florian Schrofner <fschrofner@users.noreply.github.com>
2014-06-13 18:55:19 +0200

    progress in folder sync (using normal folders). next step is to switch to app data folder.

--- src/src/at/fhhgb/mc/notify/sync/SyncHandler.java
+++ src/src/at/fhhgb/mc/notify/sync/SyncHandler.java
@@ -17,30 +17,7 @@ public class SyncHandler {
 	final static public String HOST_DESCRIPTION = "A folder used for notify syncing";
 	final static public String GOOGLE_DRIVE = "google_drive";
 	final static public String GOOGLE_DRIVE_FOLDER = "google_drive_folder_id";
-	
-	/**
-	 * Compares the local files with the files on the selected host
-	 * and returns a list of strings, which contains the file names of the
-	 * files that are missing locally.
-	 * @param _context a context which is needed for operation
-	 * @return a list of strings, containing the filenames of the missing files.
-	 */
-	static private ArrayList<String> getMissingFiles(ArrayList<String> _hostFiles,Context _context){
-		ArrayList<String> missingFiles = new ArrayList<String>();
-		File folder = new File(_context.getFilesDir()+"/"+NOTIFICATION_FOLDER); 
-		//TODO crash when folder not existent 
-		//String[] fileList = folder.list();
-		String[] fileList = {"abc"};
-		List<String> localFiles = Arrays.asList(fileList);
-		for(int i=0;i<_hostFiles.size();i++){
-			if(!localFiles.contains(_hostFiles.get(i))){
-				missingFiles.add(_hostFiles.get(i));
-				//TODO if there's a new revision of a file (if the filename contains a "_"
-				//and has a newer revision number) an update needs to take place
-			}
-		}
-		return missingFiles;
-	}
+	final static public String ROOT_NOTIFICATION_FOLDER = "/storage/emulated/0/Notify/";
 	
 	/**
 	 * Initiates an update 
--- src/src/at/fhhgb/mc/notify/sync/drive/DownloadThread.java
+++ src/src/at/fhhgb/mc/notify/sync/drive/DownloadThread.java
@@ -15,6 +15,9 @@ import com.google.api.client.http.HttpResponse;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.Drive.Files;
+import com.google.api.services.drive.Drive.Files.Get;
+import com.google.api.services.drive.model.ChildList;
+import com.google.api.services.drive.model.ChildReference;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
 
@@ -39,10 +42,10 @@ public class DownloadThread implements Runnable {
 		//TODO check for authentication, check if folder exists
 		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
 		
-		if(!preferences.contains(SyncHandler.GOOGLE_DRIVE_FOLDER) && !preferences.getString(SyncHandler.GOOGLE_DRIVE_FOLDER, "").equals("")){
-			//TODO first check if the folder exists here (created by other application)
-			createFolder();
-		}
+//		if(!preferences.contains(SyncHandler.GOOGLE_DRIVE_FOLDER) && !preferences.getString(SyncHandler.GOOGLE_DRIVE_FOLDER, "").equals("")){
+//			//TODO first check if the folder exists (created by other application)
+//			createFolder();
+//		}
 		
 		String folderId = preferences.getString(SyncHandler.GOOGLE_DRIVE_FOLDER, "");
 		
@@ -59,11 +62,43 @@ public class DownloadThread implements Runnable {
 		
 	}
 	
+	/**
+	 * Checks if there is a correct folder id set in the shared preferences,
+	 * if not a folder is created and the id is saved.
+	 * @return the id of the online folder
+	 */
 	private String checkFolder(){
-		//TODO check if there is already a notify folder in google drive
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+		//TODO change the folder from public folder to appdata folder
+		if(preferences.contains(SyncHandler.GOOGLE_DRIVE_FOLDER)){
+			try {
+				String folderId = preferences.getString(SyncHandler.GOOGLE_DRIVE_FOLDER, null);
+				Log.i(TAG, "found folder id " + folderId + "in preferences");
+				File folder = at.fhhgb.mc.notify.sync.drive.DriveHandler.service.files().get(folderId).execute();
+				if(folder != null){
+					Log.i(TAG, "confirmed online folder from preferences: " + folderId);
+					return folderId;
+				}
+			} catch (IOException e) {
+				//if folder id is not matching an online folder
+				Log.w(TAG, "found folder id, but the folder is not available on google drive!");
+			}
+		} 
+		//TODO search for folder and save in preferences
+		createFolder();
+		return preferences.getString(SyncHandler.GOOGLE_DRIVE_FOLDER, null);
+	}
+	
+	private String searchFolder(){
+		//TODO get filelist of online files and compare file names.
 		return null;
 	}
 	
+	/**
+	 * Checks if there's already an application folder inside the Google Drive.
+	 * If so, the id will be saved into the shared preferences. If not, the
+	 * folder will be created and the id will be saved.
+	 */
 	private void createFolder(){
 			try {
 				SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
@@ -74,50 +109,53 @@ public class DownloadThread implements Runnable {
 				File file;
 				file = at.fhhgb.mc.notify.sync.drive.DriveHandler.service.files().insert(body).execute();
 				String folderId = file.getId();
-				preferences.edit().putString(SyncHandler.GOOGLE_DRIVE_FOLDER, folderId);
+				preferences.edit().putString(SyncHandler.GOOGLE_DRIVE_FOLDER, folderId).commit();
+				Log.i(TAG, "created online folder with id: " + folderId);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 	}
 	
-	private ArrayList<File> getFileList(String _folderId) throws IOException{		
-		Files.List request = at.fhhgb.mc.notify.sync.drive.DriveHandler.service.files().list();
-		//TODO get only file list from notify folder
-		//at.fhhgb.mc.notify.sync.drive.DriveHandler.service.files().get(_folderId).
+	private ArrayList<File> getFileList(String _folderId) throws IOException{	
+		String folderId = checkFolder();
 		ArrayList<File> fileList = new ArrayList<File>();
-		FileList files;
-		
-		do {
-			try {
-				files = request.execute();
-				fileList.addAll(files.getItems());
-				request.setPageToken(files.getNextPageToken());
-				for(int i = 0; i<fileList.size();i++){
-					Log.i(TAG, "got: " + fileList.get(i).getOriginalFilename());
-				}			
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} while (request.getPageToken() != null && 
-				request.getPageToken().length() > 0);	
+		if(folderId != null){
+			//gets the children of the notify subfolder
+			ChildList children = at.fhhgb.mc.notify.sync.drive.DriveHandler.service.children().list(folderId).execute();
+			ArrayList<ChildReference> childList = new ArrayList<ChildReference>();
+				try {
+					childList.addAll(children.getItems());
+					//gets the fileobject of every child
+					for(int i = 0; i<childList.size();i++){
+						fileList.add(at.fhhgb.mc.notify.sync.drive.DriveHandler.service.files().get(childList.get(i).getId()).execute());
+						Log.i(TAG, "got: " + fileList.get(i).getOriginalFilename());
+					}			
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
 		return fileList;
 	}
 	
 	private ArrayList<File> getMissingFiles(ArrayList<File> _hostFiles,Context _context){
 		ArrayList<File> missingFiles = new ArrayList<File>();
-		java.io.File folder = new java.io.File(_context.getFilesDir()+"/"+SyncHandler.NOTIFICATION_FOLDER); 
-		//TODO crash when folder not existent 
-		//String[] fileList = folder.list();
-		String[] fileList = {"abc"};
+		java.io.File rootFolder = new java.io.File(SyncHandler.ROOT_NOTIFICATION_FOLDER); 
+		java.io.File folder = new java.io.File(rootFolder+"/"+SyncHandler.NOTIFICATION_FOLDER); 
+		//creates directories when the folder is not existent
+		folder.mkdirs();
+		String[] fileList = folder.list();
 		List<String> localFiles = Arrays.asList(fileList);
 		
 		for(int i=0;i<_hostFiles.size();i++){
-			if(!localFiles.contains(_hostFiles.get(i))){
+			if(_hostFiles.get(i).getOriginalFilename() != null && !localFiles.contains(_hostFiles.get(i).getOriginalFilename())){
 				missingFiles.add(_hostFiles.get(i));
+				Log.i(TAG, "file " + _hostFiles.get(i).getOriginalFilename() + " added to missing files");
 				//TODO if there's a new revision of a file (if the filename contains a "_"
 				//and has a newer revision number) an update needs to take place
+			} else {
+				Log.i(TAG, "file " + _hostFiles.get(i).getOriginalFilename() + " already exists in filesystem (or is null)");
 			}
 		}
 		return missingFiles;
@@ -127,23 +165,35 @@ public class DownloadThread implements Runnable {
 		InputStream inputStream = null;
 		OutputStream outputStream = null;
 		 try {
-			 
-			java.io.File folder = new java.io.File(_context.getFilesDir(),SyncHandler.NOTIFICATION_FOLDER);
-			folder.mkdir();
+			java.io.File rootFolder = new java.io.File(SyncHandler.ROOT_NOTIFICATION_FOLDER); 
+			java.io.File folder = new java.io.File(rootFolder,SyncHandler.NOTIFICATION_FOLDER);
+			folder.mkdirs();
 			
 			for(int i=0;i<_files.size();i++){
-				HttpResponse resp = at.fhhgb.mc.notify.sync.drive.DriveHandler.service.getRequestFactory().
-						 buildGetRequest(new GenericUrl(_files.get(i).getDownloadUrl())).execute();
-				inputStream = resp.getContent();
-				outputStream = new FileOutputStream(new java.io.File(mContext.getFilesDir() + "/" + 
-						SyncHandler.NOTIFICATION_FOLDER, _files.get(i).getOriginalFilename()));
-				int read = 0;
-				byte[] bytes = new byte[1024];
-		 
-				while ((read = inputStream.read(bytes)) != -1) {
-					outputStream.write(bytes, 0, read);
-				}
-				Log.i(TAG, "file complete!");
+				//check for correct file
+				if(_files.get(i) != null){
+					String downloadLink = _files.get(i).getDownloadUrl(); 
+					
+					//check if the file is downloadable
+					if(downloadLink != null){
+						GenericUrl downloadUrl = new GenericUrl(downloadLink);
+						Log.i(TAG, "downloadlink created: " + downloadLink.toString());
+						HttpResponse resp = at.fhhgb.mc.notify.sync.drive.DriveHandler.service.getRequestFactory().
+								 buildGetRequest(downloadUrl).execute();
+						inputStream = resp.getContent();
+						java.io.File outputFile = new java.io.File(SyncHandler.ROOT_NOTIFICATION_FOLDER + "/" + 
+								SyncHandler.NOTIFICATION_FOLDER, _files.get(i).getOriginalFilename());
+						outputFile.createNewFile();
+						outputStream = new FileOutputStream(outputFile);
+						int read = 0;
+						byte[] bytes = new byte[1024];
+				 
+						while ((read = inputStream.read(bytes)) != -1) {
+							outputStream.write(bytes, 0, read);
+						}
+						Log.i(TAG, "file " + _files.get(i).getOriginalFilename() + " complete!");
+					}
+				}			
 			}		
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
