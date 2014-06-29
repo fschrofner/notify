4f12d22904b94a5d1b401c725ac9589df20846b2
Florian Schrofner <fschrofner@users.noreply.github.com>
2014-06-29 12:31:04 +0200

    added errorhandling & scheduling for deletethread

--- src/src/at/fhhgb/mc/notify/sync/drive/DeleteThread.java
+++ src/src/at/fhhgb/mc/notify/sync/drive/DeleteThread.java
@@ -2,11 +2,13 @@ package at.fhhgb.mc.notify.sync.drive;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import com.google.api.services.drive.model.File;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 import at.fhhgb.mc.notify.notification.Notification;
 import at.fhhgb.mc.notify.sync.SyncHandler;
@@ -20,11 +22,15 @@ public class DeleteThread implements Runnable {
 	Activity mActivity;
 	ArrayList<String> mFileList;
 	ArrayList<File> mHostFiles;
+	private boolean mFinishedDeletion;
+	private boolean mConnected;
 
 	public DeleteThread(Context _context, Activity _activity, ArrayList<String> _fileList){
 		mContext = _context;
 		mActivity = _activity;
 		mFileList = _fileList;
+		mFinishedDeletion = false;
+		mConnected = true;
 	}
 	
 	@Override
@@ -32,6 +38,7 @@ public class DeleteThread implements Runnable {
 		//TODO schedule deletion, when there's no internet connection
 		DriveHandler.setup(mContext);
 		updateFileList();
+		
 		for(int i=0; i<mFileList.size(); i++){
 			deleteFile(mFileList.get(i));
 		}
@@ -62,12 +69,58 @@ public class DeleteThread implements Runnable {
 			}
 		}	
 		oldFile = new java.io.File(SyncHandler.getFullPath(_fileName));
-		deleteHostFile(_fileName);
+		
+		mFinishedDeletion = false;
+		do{
+			deleteHostFile(_fileName);
+			if(!mFinishedDeletion && mConnected){
+				try {
+					Thread.sleep(10000);
+					Log.i(TAG, "deletion failed! waiting for 10 seconds and retry");
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			//internet connectivity is not present, don't retry but schedule the deletion
+			if(!mConnected){
+				ArrayList<String> deleteFile = new ArrayList<String>();
+				deleteFile.add(_fileName);
+				scheduleDeletion(deleteFile);
+				break;
+			}
+		} while(!mFinishedDeletion);
+		
 		if(oldFile != null){
 			oldFile.delete();
 			Log.i(TAG, "file " + _fileName + " was deleted from the local filesystem");
 		}			
 	}
+	
+	private void scheduleDeletion(ArrayList<String> _fileList){
+		SharedPreferences outstanding = mContext.getSharedPreferences(SyncHandler.OUTSTANDING_TASKS, Context.MODE_PRIVATE); 
+		HashSet<String> redoFileList;
+		
+		//if already other deletions are scheduled, just append the new file
+		if(outstanding.contains(SyncHandler.OUTSTANDING_DELETION)){
+			redoFileList = (HashSet<String>) outstanding.getStringSet(SyncHandler.OUTSTANDING_DELETION, null);
+		} else {
+			redoFileList = new HashSet<String>();
+		}
+		
+		for(int i=0; i<_fileList.size();i++){
+			//adds the file to the missing uploads, if not present yet
+			if(!redoFileList.contains(_fileList.get(i))){
+				redoFileList.add(_fileList.get(i));
+				Log.i(TAG, "added the file " + _fileList.get(i) + " to the scheduled deletions");
+			} else {
+				Log.i(TAG, "deletion of file " + _fileList.get(i) + " already scheduled");
+			}
+		}
+		
+		outstanding.edit().putStringSet(SyncHandler.OUTSTANDING_DELETION, redoFileList).commit();
+	}
+	
 	
 	private void deleteHostFile(String _fileName){
 		for(int i=0; i<mHostFiles.size(); i++){
@@ -77,11 +130,19 @@ public class DeleteThread implements Runnable {
 					DriveHandler.service.files().delete(id).execute();
 					Log.i(TAG, "file " + _fileName + " deleted on the host filesystem");
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					//TODO schedule a re-upload, if network error. save files to upload, if internet connectivity is deactivated.	
+		        	Log.w(TAG, "network error!");
+		        	boolean isConnected = SyncHandler.networkConnected(mContext);
+		        	if(!isConnected){
+		        		//TODO register broadcast receiver for network connectivity changed and save files to upload in shared preferences
+		        		mConnected = false;
+		        		Log.i(TAG, "no internet connectivity! scheduled upload for connectivity change");
+		        	}
 				}
 			}
 		}
+		mConnected = SyncHandler.networkConnected(mContext);
+		mFinishedDeletion = true;	
 	}
 	
 	private void updateFileList(){	
